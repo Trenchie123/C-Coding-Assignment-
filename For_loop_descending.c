e03f032b64cf2bb0c8b5a00ed6e416a8cccdeac5
Trenchie123 <martinrahif6@gmail.com>
2025-10-09 09:59:58 +0300

    Add files via upload

---
+++ For_loop_descending.c
@@ -0,0 +1,24 @@
+// A For loop
+/* 
+Name : Martin Ndegwa Kagechu 
+Reg No: CT100/G /26201 /25
+Des: using For loop to count numbers from 100 to 50 in descending order
+*/
+
+#include <stdio.h>
+
+int main() {
+       int i;
+       
+       for ( i=100; i>=50; i--){
+       printf("%d\n", i);
+       }
+        
+       return 0;
+}
+
+
+
+
+
+
